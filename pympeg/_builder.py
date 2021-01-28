e3172327466d3e8de5da364b64028d006c3c7259
AP-Atul <atulpatare99@gmail.com>
2021-01-28 20:40:16 +0530

    cmd gen updated

--- pympeg/_builder.py
+++ pympeg/_builder.py
@@ -2,6 +2,85 @@
 from ._util import *
 from ._node import *
 
+def get_str_from_params(params: dict):
+    """ Returns string from the parameters """
+    result = list()
+    keys = list(params.keys())
+    length = len(keys)
+
+    result.append("%s=%s" % (keys[0], params[keys[0]]))
+    for i in range(1, length):
+        result.append(":%s=%s" % (keys[i], params[keys[i]]))
+
+    return ''.join(result)
+
+
+def get_str_from_filter(filter):
+    """ Returns the string from the filter """
+    result = list()
+
+    for inp in filter.inputs:
+        result.append("[%s]" % inp.label)
+
+    result.append(" %s=%s " % (filter.name, get_str_from_params(filter.params)))
+
+    for out in filter.outputs:
+        result.append("[%s]" % out.label)
+
+    result.append(";")
+
+    return ''.join(result)
+
+
+def get_str_from_global(node):
+    """ Returns the string from the global node """
+    result = list()
+
+    for inp in node.inputs:
+        result.append("[%s]" % inp.label)
+
+    result.append("%s" % node.name)
+
+    for out in node.outputs:
+        result.append("[%s]" % out.label)
+
+    result.append(";")
+
+    return ''.join(result)
+
+
+def get_str_from_input(node):
+    result = list()
+    result.append("-i %s" % node.name)
+    return ' '.join(result)
+
+
+def get_str_from_output(node):
+    result = list()
+    for inp in node.inputs:
+        result.append('%s "[%s]"' % (node.map, inp.label))
+    result.append(" %s " % node.name)
+    return ' '.join(result)
+
+
+def get_str_from_option(node):
+    result = list()
+    result.append(" %s %s" % (node.tag, node.name))
+    return ' '.join(result)
+
+def get_str_from_node(node):
+    if isinstance(node, InputNode):
+        return get_str_from_input(node)
+    if isinstance(node, OptionNode):
+        return get_str_from_option(node)
+    if isinstance(node, FilterNode):
+        return get_str_from_filter(node)
+    if isinstance(node, GlobalNode):
+        return get_str_from_global(node)
+    if isinstance(node, OutputNode):
+        return get_str_from_output(node)
+
+
 class Stream:
     """
     Simple list manager, adds nodes and returns the chain
@@ -9,9 +88,10 @@ class Stream:
     nodes.
     """
     def __init__(self):
-        self._chain = list()
+        self._chain, self.count = list(), 0
     def add(self, data):
         self._chain.append(data)
+        return self
     def graph(self):
         return self._chain
 
