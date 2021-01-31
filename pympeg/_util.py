7763dd999ed445477b84247495345280bee08886
atul <atulpatare99@gmail.com>
2021-01-31 22:42:33 +0530

    more clean ups

--- pympeg/_util.py
+++ pympeg/_util.py
@@ -3,12 +3,9 @@
 import string
 from random import sample
 
-
 def gen_labels(n=1):
     """ Randomly generate label of size 3 with alphabets """
     length = 3
     if n == 1:
         return ''.join(sample(string.ascii_letters, length))
-
     return [''.join(sample(string.ascii_letters, length)) for _ in range(n)]
-
